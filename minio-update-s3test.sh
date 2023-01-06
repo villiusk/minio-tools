f852e0a797ca4bb95bff60351f2495598fa0f920
villiusk <40464366+villiusk@users.noreply.github.com>
2023-01-06 10:43:39 +0200

    Update minio-update-s3test.sh

--- minio-update-s3test.sh
+++ minio-update-s3test.sh
@@ -7,31 +7,40 @@ echo "$0 must be run as root";
 exit 1;
 fi
 
-MINIO_BIN_URL="https://s3test.bluebridge.lt/minio1/minio"
 INSTALLED_RELEASE=` minio -v | grep -o -P 'RELEASE.+\s' | tr -s ''`
-INSTALLED_MD5=`md5sum /usr/local/bin/minio | cut -d " " -f 1`
 
-S3TEST_MD5=`curl -I -s $MINIO_BIN_URL | grep ETag | cut -d ' ' -f 2 | tr -d '"'`
+MINIO_BIN_URL="https://s3test.bluebridge.lt/tested-minio/minio"
+MINIO_BIN_SHA256="https://s3test.bluebridge.lt/tested-minio/minio.sha256"
 
-#CURRENT_MINIO_SHA256SUM=`curl https://dl.min.io/server/minio/release/linux-amd64/minio.sha256sum`
-#CURRENT_MINIO_SHA256SUM=`echo $CURRENT_MINIO_SHA256SUM | cut -d ' ' -f 1`
+CURRENT_MINIO_SHA256SUM=`curl $MINIO_BIN_SHA256`
+CURRENT_MINIO_SHA256SUM=`echo $CURRENT_MINIO_SHA256SUM | cut -d ' ' -f 1`
 
-#INSTALLED_MINIO_SHA256SUM=`sha256sum /usr/local/bin/minio`
-#INSTALLED_MINIO_SHA256SUM=`echo $INSTALLED_MINIO_SHA256SUM | cut -d ' ' -f 1`
+INSTALLED_MINIO_SHA256SUM=`sha256sum /usr/local/bin/minio`
+INSTALLED_MINIO_SHA256SUM=`echo $INSTALLED_MINIO_SHA256SUM | cut -d ' ' -f 1`
+
+if [ $CURRENT_MINIO_SHA256SUM != $INSTALLED_MINIO_SHA256SUM ]; then
+    echo "New MINIO realese exists and will be downloaded"
+    rm -f minio
+    wget https://dl.min.io/server/minio/release/linux-amd64/minio
+    chmod +x minio
+
+    echo "$CURRENT_MINIO_SHA256SUM minio" > minio.sha256sum
 
 
-echo "Instaled minio MD5: "
-echo $INSTALLED_MD5;
-echo;
-echo "S3test MD5: "
-echo $S3TEST_MD5;
+    if sha256sum -c minio.sha256sum | grep -q ': OK' ; then
+    echo "copiing new Minio "
 
-if [ $INSTALLED_MD5 != $S3TEST_MD5 ]; then
-echo "New version will be installed";
+    #mkdir -p old_minio/$INSTALLED_RELEASE
+    #cp -f /usr/local/bin/minio old_minio/$INSTALLED_RELEASE
+    #echo $INSTALLED_RELEASE > old_minio/pre_version
 
+    setcap 'cap_net_bind_service=+ep' /usr/local/bin/minio
+    cp -f minio /usr/local/bin/ && systemctl restart minio
+
+    #gzip old_minio/$INSTALLED_RELEASE/minio
+
+    fi
 else
-echo "System already use latest trusted version from s3test"; 
-echo;
-
-
+    echo "You have latest version of MINIO binary on your system"
 fi
+
